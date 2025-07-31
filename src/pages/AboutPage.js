60ca90800b651e237a9e87e9bb94cc215642e3f8
phu <trieuphu130404@gmail.com>
2025-07-31 14:44:24 +0700

    giao diện trang about

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>Giới thiệu về nhóm</h1>
+      <p>Chúng tôi là nhóm phát triển ứng dụng phim ...</p>
+      <ul>
+        <li>Thành viên A: Lê Trần Triệu Phú</li>
+        <li>Thành viên B: Nguyễn Khải Hưng</li>
+        <li>Thành viên C: Nguyễn Nhật Hào</li>
+      </ul>
+    </div>
+  );
+}
+
+export default AboutPage;
